258abbf09471b8e63247e308fee015bc335eece6
IAMSUTHICHA <suthicha@outlook.com>
2018-02-27 11:50:30 +0700

    add IO package

---
+++ SPKHelperPackage/IO/AsDirectory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SPKHelperPackage.IO
+{
+    public interface IAsDirectory
+    {
+        void Create(string path, string folderName);
+
+        void Trash(string sourceFolderWithPath);
+
+        List<FileInfo> GetFiles(string path, string ext = "*.*");
+    }
+
+    public class AsDirectory : IAsDirectory
+    {
+        public void Create(string path, string folderName)
+        {
+            try
+            {
+                string destFolder = Path.Combine(path, folderName);
+
+                if (!Directory.Exists(destFolder))
+                {
+                    Directory.CreateDirectory(destFolder);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Trash(string sourceFolderWithPath)
+        {
+            try
+            {
+                Directory.Delete(sourceFolderWithPath, true);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<FileInfo> GetFiles(string path, string ext = "*.*")
+        {
+            return new DirectoryInfo(path).GetFiles(ext).ToList();
+        }
+    }
+}
---
+++ SPKHelperPackage/IO/AsFile.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace SPKHelperPackage.IO
+{
+    public interface IAsFile
+    {
+        byte[] ReadToByteArray(string fileName);
+
+        bool Delete(string fileName);
+
+        void WriteFile(string fileName, string content);
+    }
+
+    public class AsFile : IAsFile
+    {
+        public bool Delete(string fileName)
+        {
+            try
+            {
+                File.Delete(fileName);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public byte[] ReadToByteArray(string fileName)
+        {
+            return toByteArray(fileName);
+        }
+
+        public void WriteFile(string fileName, string content)
+        {
+            using (StreamWriter sw = new StreamWriter(
+             new FileStream(fileName, FileMode.Append)))
+            {
+                try
+                {
+                    sw.WriteLine(content);
+                }
+                catch { }
+            }
+        }
+
+        private byte[] toByteArray(string fileName)
+        {
+            byte[] buffer = null;
+
+            try
+            {
+                using (FileStream fs = File.OpenRead(fileName))
+                {
+                    using (BinaryReader binaryReader = new BinaryReader(fs))
+                    {
+                        buffer = binaryReader.ReadBytes((int)fs.Length);
+                    }
+                }
+            }
+            catch { }
+
+            return buffer;
+        }
+    }
+}
